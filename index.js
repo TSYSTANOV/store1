264d868bd36ea275d7b561a97b8b3abaf72b744e
TSYSTANOV <tsystanov.vitaly@gmail.com>
2024-01-24 17:33:26 +0200

    add to cart - logika

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 ///
 let productCart = [];
 let activeId = "";
-
+let index = 1
 getCategories();
 async function getCategories() {
   await getUllCategories();
@@ -29,9 +29,13 @@ async function getCategories() {
       event.target.classList.add("activebBtn");
       if (activeId === "All") {
         getAllProduct();
-      } else {
+      } else if(activeId === "cart"){
+        renderCart(productCart)
+      }
+      else{
         getProductsByCategory(activeId);
       }
+      
       console.log(activeId);
     });
   });
@@ -123,16 +127,153 @@ async function getProductsByCategory(category) {
 }
 
 function addToCart(product, elem) {
+
   return () => {
+    
+    let nonVisibleDiv = document.createElement('div')
+    nonVisibleDiv.className = 'nonVisibleDiv'
+    ///
+    let div = document.createElement('div')
+    div.className = 'modal-product'
+    let span = document.createElement('span')
+    span.className = 'modal-cart-close'
+    span.textContent = 'X'
+    span.addEventListener('click',()=>
+    {
+      document.querySelector('.nonVisibleDiv').remove()  
+    })
+    div.append(span)
+
+    let div2 = document.createElement('div')
+    div2.className = 'product-cart-modal'
+
+    let spanPlus = document.createElement('span')
+    spanPlus.className = 'numbers'
+    spanPlus.textContent = '+'
+    spanPlus.addEventListener('click', changeCount(true))
+
+    let spanMinus = document.createElement('span')
+    spanMinus.className = 'numbers'
+    spanMinus.textContent = '-'
+    spanMinus.addEventListener('click', changeCount(false))
+    
+    let div3 = document.createElement('div')
+    div3.className = 'product-info-modal'
+    div3.innerHTML = `
+    <h3>${product.title}</h3>
+    <p>Rating: ${product.rating.rate}</p>
+    <p>${product.description}</p>
+    <div class="product-btns">
+    <p class='countForCart'>${index}</p>
+    </div>
+    <p>Цена: ${product.price} USD</p>`
+
+    div2.innerHTML = `
+    <div class='product-image-modal'>
+    <img src='${product.image}'/></div`
+
+    let btn = document.createElement('button')
+    btn.className = 'addTocartModal'
+    btn.textContent = 'Add to cart'
+    btn.addEventListener('click', clickToCartHandler(product,elem))
+
+    div3.querySelector('.product-btns').append(spanPlus)
+    div3.querySelector('.product-btns').prepend(spanMinus)
+
+    div3.append(btn)
+
+    div2.append(div3)
+
+    div.append(div2)
+    nonVisibleDiv.append(div)
+    document.querySelector('.container').prepend(nonVisibleDiv)
+  };
+}
+function changeCount(ident)
+{
+  return()=>
+  {
+    if(ident)
+    {index++
+    document.querySelector('.countForCart').innerHTML = index
+    numb = index
+    }
+    if(!ident)
+    { 
+      if(index >1)
+      {
+      index--
+      document.querySelector('.countForCart').innerHTML = index
+      }
+    }
+  }
+}
+function clickToCartHandler(product,elem)
+{
+  return()=>
+  { 
     elem.textContent = "🔥 In Cart";
     elem.closest(".item").style.backgroundColor = "pink";
     elem.style.pointerEvents = "none";
-    console.log(elem);
-    productCart.push(product);
-    renderCart(productCart);
-  };
+
+    document.querySelector('.nonVisibleDiv').style.cssText = `
+    transition:0.5s;
+    transform:scale(0);
+    `
+    setTimeout(()=>
+    {
+      document.querySelector('.nonVisibleDiv').remove()
+    },500)
+  
+    let createProduct = {...product,count:index}
+   
+  }
 }
 
 function renderCart(array) {
-  console.log(array);
+
+  let nonVisibleDiv = document.createElement('div')
+  nonVisibleDiv.className = 'nonVisibleDiv'
+
+
+  let div = document.createElement('div')
+  div.className = 'modal-cart'
+  let span = document.createElement('span')
+  span.className = 'modal-cart-close'
+  span.textContent = 'X'
+  span.addEventListener('click',()=>
+  {
+    document.querySelector('.nonVisibleDiv').remove()  
+  })
+  div.append(span)
+  
+  let table = document.createElement('table')
+  table.className = 'table'
+  div.append(table)
+
+  for(let i = 0 ; i <array.length;i++)
+  {
+    let product = array[i]
+
+    let div2 = document.createElement('tr')
+    div2.className = 'modal-cart-item'
+    div2.innerHTML = 
+    `
+    <td><img class='modal-cart-item-image' src='${product.image}'/></td>
+    <td class='modal-cart-item-title'>${product.title}</td>
+    <td class='modal-cart-item-price'>${product.price} USD</td>
+    <td class='modal-cart-btns'>
+    <span class='numbers'>-</span>
+    <p class='modal-cart-item-count'>${product.count}</p>
+    <span class='numbers'>+</span></td>
+    <td>Summary: USD</td>
+    `
+    table.append(div2)
+  }
+  let p = document.createElement('p')
+  p.className = 'total-price'
+  p.textContent = 'Total: USD'
+  div.append(p)
+  nonVisibleDiv.append(div)
+  document.querySelector('.container').prepend(nonVisibleDiv)
 }
